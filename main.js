5bb600e6cab90171af23d879f0368c2e37e27e64
CRISvsGAME <cris@crisvsgame.com>
2025-01-04 20:00:00 +0000

    3DCSS 0.0.4
    Add Body Client Width and Height

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ const showInfo = () => {
   htmlClientWidth.textContent = document.documentElement.clientWidth;
   const htmlClientHeight = document.getElementById("html-client-height");
   htmlClientHeight.textContent = document.documentElement.clientHeight;
+
+  const bodyClientWidth = document.getElementById("body-client-width");
+  bodyClientWidth.textContent = document.body.clientWidth;
+  const bodyClientHeight = document.getElementById("body-client-height");
+  bodyClientHeight.textContent = document.body.clientHeight;
 };
 
 window.addEventListener("load", () => {
