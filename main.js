3dbeb9fdc13583f9964287374b911cc07f6942af
CRISvsGAME <cris@crisvsgame.com>
2026-01-04 20:00:00 +0000

    3DCSS 0.0.5
    Adjust Indentation

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 const showInfo = () => {
-  const windowInnerWidth = document.getElementById("window-inner-width");
-  windowInnerWidth.textContent = window.innerWidth;
-  const windowInnerHeight = document.getElementById("window-inner-height");
-  windowInnerHeight.textContent = window.innerHeight;
+    const windowInnerWidth = document.getElementById("window-inner-width");
+    windowInnerWidth.textContent = window.innerWidth;
+    const windowInnerHeight = document.getElementById("window-inner-height");
+    windowInnerHeight.textContent = window.innerHeight;
 
-  const htmlClientWidth = document.getElementById("html-client-width");
-  htmlClientWidth.textContent = document.documentElement.clientWidth;
-  const htmlClientHeight = document.getElementById("html-client-height");
-  htmlClientHeight.textContent = document.documentElement.clientHeight;
+    const htmlClientWidth = document.getElementById("html-client-width");
+    htmlClientWidth.textContent = document.documentElement.clientWidth;
+    const htmlClientHeight = document.getElementById("html-client-height");
+    htmlClientHeight.textContent = document.documentElement.clientHeight;
 
-  const bodyClientWidth = document.getElementById("body-client-width");
-  bodyClientWidth.textContent = document.body.clientWidth;
-  const bodyClientHeight = document.getElementById("body-client-height");
-  bodyClientHeight.textContent = document.body.clientHeight;
+    const bodyClientWidth = document.getElementById("body-client-width");
+    bodyClientWidth.textContent = document.body.clientWidth;
+    const bodyClientHeight = document.getElementById("body-client-height");
+    bodyClientHeight.textContent = document.body.clientHeight;
 };
 
 window.addEventListener("load", () => {
-  showInfo();
+    showInfo();
 });
 
 window.addEventListener("resize", () => {
-  showInfo();
+    showInfo();
 });
