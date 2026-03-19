087d7ed947212e4cad36f582012a56e375362edd
CRISvsGAME <cris@crisvsgame.com>
2026-03-19 20:00:00 +0000

    3DCSS 0.0.41
    Add JSDoc

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
+// @ts-check
+
 const showInfo = () => {
     const windowInnerWidth = document.getElementById("window-inner-width");
-    windowInnerWidth.textContent = f(window.innerWidth);
+    if (windowInnerWidth) windowInnerWidth.textContent = f(window.innerWidth);
     const windowInnerHeight = document.getElementById("window-inner-height");
-    windowInnerHeight.textContent = f(window.innerHeight);
+    if (windowInnerHeight) windowInnerHeight.textContent = f(window.innerHeight);
 
     const htmlClientWidth = document.getElementById("html-client-width");
-    htmlClientWidth.textContent = f(document.documentElement.clientWidth);
+    if (htmlClientWidth) htmlClientWidth.textContent = f(document.documentElement.clientWidth);
     const htmlClientHeight = document.getElementById("html-client-height");
-    htmlClientHeight.textContent = f(document.documentElement.clientHeight);
+    if (htmlClientHeight) htmlClientHeight.textContent = f(document.documentElement.clientHeight);
 
     const bodyClientWidth = document.getElementById("body-client-width");
-    bodyClientWidth.textContent = f(document.body.clientWidth);
+    if (bodyClientWidth) bodyClientWidth.textContent = f(document.body.clientWidth);
     const bodyClientHeight = document.getElementById("body-client-height");
-    bodyClientHeight.textContent = f(document.body.clientHeight);
+    if (bodyClientHeight) bodyClientHeight.textContent = f(document.body.clientHeight);
 };
 
+/**
+ * @param {number} n
+ * @returns {string}
+ */
 const f = (n) => String(n).padStart(4, "0");
 
 window.addEventListener("load", () => {
